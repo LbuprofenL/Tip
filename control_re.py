639780d5063e16c97365159d0ce59262296472a2
LbuprofenL <1248636101@qq.com>
2023-03-06 23:01:20 +0800

    Time to sleep

--- control_re.py
+++ control_re.py
@@ -2,7 +2,8 @@ import serial
 import struct
 import tools
 import serial.tools.list_ports
-
+PKG_SIZE = 5
+addr = 1
 
 class Tip:
     tip_count = 0
@@ -10,7 +11,7 @@ class Tip:
     def __init__(self):
         '''初始化串口'''
         if Tip.tip_count == True:
-            return False
+            raise Exception("超过最多打开设备数量.")
 
         ports_list = list(serial.tools.list_ports.comports())
         if len(ports_list) <= 0:
@@ -39,40 +40,115 @@ class Tip:
             return False
         return True
 
-    def check(self):
-        '''接收返回状态码并处理'''
-
+    def recv(self):
+        '''监听返回的数据'''
+        if self.ports.is_open:
+            pkg = self.ports.read(PKG_SIZE)
+            print(pkg)
+            return pkg
+        else:
+            raise Exception
+        
+        
     def It(self, vel=16000, power=100, mode=0):
             '''初始化移液枪位置'''
             msg = "It" + str(vel) + "," + str(power) + "," + str(mode)
             self.send(msg)
+            try:
+                pkg = self.recv()
+                try:
+                    status, data = tools.Unpack(pkg,addr)
+                    try: 
+                        tools.check_status(status)
+                    except:
+                        print("")
+                except:
+                    print("解包出错,请重试.")
+            except:
+                print("端口未打开,请打开后重试.")
+            
 
     def Ia(self, vol, imbi_vel=500, stop_vel=10):
             '''吸液'''
             msg = "Ia" + str(vol) + "," + str(imbi_vel) + "," + str(stop_vel)
             self.send(msg)
+            try:
+                pkg = self.recv()
+                try:
+                    status, data = tools.Unpack(pkg, addr)
+                    try:
+                        tools.check_status(status)
+                    except:
+                        print("")
+                except:
+                    print("解包出错,请重试.")
+            except:
+                print("端口未打开,请打开后重试.")
 
     def Da(self, vol, back_vol=0, run_vel=500, stop_vel=10):
             # 排液
             msg = "Da" + str(vol) + "," + str(back_vol) + \
                 "," + str(run_vel) + "," + str(stop_vel)
             self.send(msg)
+            try:
+                pkg = self.recv()
+                try:
+                    status, data = tools.Unpack(pkg, addr)
+                    try:
+                        tools.check_status(status)
+                    except:
+                        print("")
+                except:
+                    print("解包出错,请重试.")
+            except:
+                print("端口未打开,请打开后重试.")
 
     def Dt(self, vel=16000, mode=0):
             # 排除tip头
             msg = "Dt" + str(vel) + "," + str(mode)
             self.send(msg)
+            try:
+                pkg = self.recv()
+                try:
+                    status, data = tools.Unpack(pkg, addr)
+                    try:
+                        tools.check_status(status)
+                    except:
+                        print("")
+                except:
+                    print("解包出错,请重试.")
+            except:
+                print("端口未打开,请打开后重试.")
 
     def Ld(self, mode=1, timeout=10000):
-            # 页面检测
+            # 液面检测
+            # TODO 模式0
             msg = "Ld" + str(mode) + "," + str(timeout)
             self.send(msg)
+            if mode == 1:
+                try:
+                    pkg = self.recv()
+                    try:
+                        status, data = tools.Unpack(pkg, addr)
+                        try:
+                            tools.check_status(status)
+                        except:
+                            print("")
+                    except:
+                        print("解包出错,请重试.")
+                except:
+                    print("端口未打开,请打开后重试.")
+            else:
+                # TODO
+                return
 
 
 if __name__ == '__main__':
     tip = Tip()
     tip.open_serial()
-    tip.It(16000, 100, 0)
-    tip.Ia(10000,200,10)
-    tip.Da(1000,500,200,100)
-    tip.Ld(1,5000)
+    for i in range(0,2):
+        tip.It(16000, 100, 0)
+        
+    # tip.Ia(10000,200,10)
+    # tip.Da(1000,500,200,100)
+    # tip.Ld(1,5000)
--- tools.py
+++ tools.py
@@ -1,25 +1,119 @@
 import struct
 
+
 def check_sum(msg):
-        
+
     check = sum(msg)  # 计算校验和
     check = check & 0xff  # 与计算 保留后8位
-    #check = check.to_bytes(1, "big")  # 转化为1个字节
+    # check = check.to_bytes(1, "big")  # 转化为1个字节
     return check
 
 
 def Pack_command(msg, addr):
-	values = (170, addr, len(msg), msg.encode("utf-8"))
-	#print(values)
+    '''将指令封装成帧'''
+    values = (170, addr, len(msg), msg.encode("utf-8"))
+    # print(values)
 
-	fmt = "> B B B" + str(len(msg))+"s"  # 格式
+    fmt = "> B B B" + str(len(msg))+"s"  # 格式
 
-	packed_data = struct.pack(fmt, *values)
+    packed_data = struct.pack(fmt, *values)
 
-	check = check_sum(packed_data).to_bytes(1,"big")
-	packed_data = packed_data + check
-	#print(packed_data)
+    check = check_sum(packed_data).to_bytes(1, "big")
+    packed_data = packed_data + check
+    # print(packed_data)
 
-	return(packed_data)
+    return(packed_data)
 
-# TODO 设计解包程序
+
+def Unpack(pkg, addr=1):
+    "将返回的帧解包"
+    fmt = "> B B B B" + str((len(pkg)-5)) + "s B"
+    msg = struct.unpack_from(fmt, pkg)
+
+    # 校验帧头
+    if msg[0] != 85:
+        raise Exception
+    # 校验地址
+    if addr != msg[1]:
+        raise Exception
+    # 判断返回数据长度
+    if len(pkg)-5 == 0:
+        check_fmt = "> B B B B"
+    else:
+        check_fmt = "> B B B B" + str(len(pkg)-5) + "s"
+
+    check_values = msg[:len(pkg)-1]
+    check_msg = struct.pack(check_fmt, *check_values)
+    # 检验校验和
+    if msg[len(pkg)] != check_sum(check_msg):
+        raise Exception
+    # 返回状态处理
+    status = msg[2]
+    # 返回数据
+    data_len = msg[3]
+    data = str(msg[4:4+data_len-1])
+    # print(type(data))
+    return status, data
+
+
+def check_status(status):
+    '''接收返回状态码并处理'''
+    if status in range(0, 9):
+        if status == 0:
+            print("设备空闲")
+        elif status == 1:
+            print("设备忙")
+        elif status == 2:
+            print("执行成功")
+        elif status == 3:
+            print("执行完成")
+        elif status == 4:
+            print("探测到液面")
+        else:
+            print("未定义状态值")
+    elif status in range(10, 19):
+        if status == 10:
+            print("参数超出范围")
+        elif status == 11:
+            print("参数错误")
+        elif status == 12:
+            print("指令语法错误")
+        elif status == 13:
+            print("无效指令")
+        elif status == 14:
+            print("地址错误")
+        elif status == 15:
+            print("禁止写")
+        elif status == 16:
+            print("禁止读")
+        elif status == 17:
+            print("未初始化")
+        else:
+            print("未定义状态值")
+    elif status >= 20:
+        if status == 20:
+            print("未检测到TIP头报警")
+        elif status == 21:
+            print("顶出TIP头报警")
+        elif status == 22:
+            print("超时报警")
+        elif status == 23:
+            print("吸气凝块检测报警")
+        elif status == 24:
+            print("吸气气泡检测报警")
+        elif status == 25:
+            print("吸气空检测报警")
+        elif status == 50:
+            print("电机堵转错误")
+        elif status == 51:
+            print("驱动器故障")
+        elif status == 52:
+            print("零位光耦故障")
+        elif status == 53:
+            print("TIP头光耦错误")
+        elif status == 54:
+            print("传感器错误")
+        elif status == 55:
+            print("存储错误")
+        else:
+            print("未定义状态值")
